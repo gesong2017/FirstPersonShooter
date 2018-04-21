e7a64d5e18b12f7cd753590dd7a0a6a67d798ff0
DESKTOP-76F9ME7\Rorschach <gesong2017@gmail.com>
2018-04-21 01:49:02 -0400

    Empty Project Created

---
+++ OverWatch/Source/OverWatch/OverWatchGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OverWatchGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERWATCH_API AOverWatchGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
